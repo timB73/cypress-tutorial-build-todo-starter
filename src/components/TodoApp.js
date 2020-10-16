1bbe5da519bc853b1988c1720bd0c19384bb93b3
psytb5 <psytb5@nottingham.ac.uk>
2020-10-16 16:33:10 +0100

    Done tutorial 7 - filters and data-driven tests

--- src/components/TodoApp.js
+++ src/components/TodoApp.js
@@ -4,6 +4,7 @@ import TodoForm from './TodoForm';
 import TodoList from './TodoList';
 import Footer from './Footer';
 import { destroyTodo, loadTodos, saveTodo, updateTodo } from '../lib/service';
+import { filterTodos } from '../lib/utils';
 
 export default class TodoApp extends Component {
   constructor(props) {
@@ -78,10 +79,15 @@ export default class TodoApp extends Component {
             />
           </header>
           <section className="main">
-            <TodoList
-              todos={this.state.todos}
-              handleDelete={this.handleDelete}
-              handleToggle={this.handleToggle}
+            <Route
+              path="/:filter?"
+              render={({ match }) => (
+                <TodoList
+                  todos={filterTodos(match.params.filter, this.state.todos)}
+                  handleDelete={this.handleDelete}
+                  handleToggle={this.handleToggle}
+                />
+              )}
             />
           </section>
           <Footer remaining={remaining} />
