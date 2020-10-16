e6f45c1043b2d1eb227395fdd83bfcd7ccd5920f
psytb5 <psytb5@nottingham.ac.uk>
2020-10-16 11:19:20 +0100

    Finished tutorial 4

--- cypress/integration/app-init.spec.js
+++ cypress/integration/app-init.spec.js
@@ -1,31 +1,23 @@
-const todos = [
-  {
-    id: 1,
-    name: "Buy Milk",
-    isComplete: false,
-  },
-  {
-    id: 2,
-    name: "Buy Eggs",
-    isComplete: false,
-  },
-  {
-    id: 3,
-    name: "Buy Bread",
-    isComplete: false,
-  },
-  {
-    id: 4,
-    name: "Make French Toast",
-    isComplete: false,
-  },
-];
 describe("App initialization", () => {
-  it.only("Loads todos on page load", () => {
-    cy.server();
-    cy.route("GET", "/api/todos", todos);
-    cy.visit("/");
+  it("Loads todos on page load", () => {
+    cy.seedAndVisit();
 
     cy.get(".todo-list li").should("have.length", 4);
+  });
+
+  it("Displays an error message on failure", () => {
+    cy.server();
+    cy.route({
+      url: "/api/todos",
+      method: "GET",
+      status: 500,
+      response: {},
+    });
+
+    cy.visit("/");
+
+    cy.get(".todo-list li").should("not.exist");
+
+    cy.get(".error").should("be.visible");
   });
 });
